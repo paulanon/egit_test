2edf9e8842c8cb182fe5ab08cd516eb32c67c252
Paul AnonSiamEgit <paulanon+siam_egit@gmail.com>
2011-09-08 13:52:12 -0700

    first commit

---
+++ EclipseProjects/Blank/src/org/bla/baba.java
@@ -0,0 +1,47 @@
+package org.bla;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.SpringLayout;
+
+//in C:usersykilgitEclipseBlanksrcorgbla
+
+public class baba {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					baba window = new baba();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public baba() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new SpringLayout());
+	}
+
+}
